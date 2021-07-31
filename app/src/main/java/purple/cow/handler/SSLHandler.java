730a8a02d66615bf464a003f0f9dff4c9949df39
bzack2012 <bzachary12@gmail.com>
2021-07-31 13:51:48 -0400

    Added some files to gitignore. Made some dependency changes to remove a runtime error when deploying sam locally.

--- app/src/main/java/purple/cow/handler/SSLHandler.java
+++ app/src/main/java/purple/cow/handler/SSLHandler.java
@@ -5,25 +5,23 @@ import com.amazonaws.serverless.proxy.model.AwsProxyRequest;
 import com.amazonaws.serverless.proxy.model.AwsProxyResponse;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
-import com.fasterxml.jackson.core.util.JacksonFeature;
 import org.glassfish.jersey.server.ResourceConfig;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
-
+/**
+ * SSL Handler class for our Purple Cow API
+ * Implements the RequestStreamHandler Interface from AWS Lambda for
+ * deployment as a Lambda Function
+ */
 public class SSLHandler implements RequestStreamHandler {
 
     private static final ResourceConfig jerseyApplication = new ResourceConfig()
-            .packages("purple.cow.handler")
-            .register(JacksonFeature.class);
+            .packages("purple.cow.resources");
     private static final JerseyLambdaContainerHandler<AwsProxyRequest, AwsProxyResponse> handler
             = JerseyLambdaContainerHandler.getAwsProxyHandler(jerseyApplication);
-    // If you are using HTTP APIs with the version 2.0 of the proxy model, use the getHttpApiV2ProxyHandler
-    // method:
-    // JerseyLambdaContainerHandler<HttpApiV2ProxyRequest, AwsProxyResponse> handler =
-    //        JerseyLambdaContainerHandler.getHttpApiV2ProxyHandler(jerseyApplication);
 
     @Override
     public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context)
--- app/src/main/java/purple/cow/resources/SSLResource.java
+++ app/src/main/java/purple/cow/resources/SSLResource.java
@@ -6,12 +6,15 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import java.security.cert.X509Certificate;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
-import java.util.Enumeration;
 import java.util.Objects;
 
+/**
+ * Purple Cow API. Functionality includes testing the certificates of users to see if their certs have expired.
+ */
 @Path("/purple-cow")
 public class SSLResource {
 
@@ -21,31 +24,40 @@ public class SSLResource {
     public static final String CORRUPTED_CERT_MESSAGE = "Your certificate does not have a valid date or is corrupted." +
             "Please obtain a new certificate.";
 
-    @Path("/purplecowTest") @GET
-    @Produces(MediaType.TEXT_PLAIN)
+    /**
+     * Test Endpoint to validate purple-cow api is up and running.
+     *
+     * @param context
+     * @return String
+     */
+    @GET
+    @Path("/purpleCowTest")
+    @Produces(MediaType.APPLICATION_JSON)
     public String purpleCowTest(@Context HttpServletRequest context) {
-        Enumeration<String> headerNames = context.getHeaderNames();
-        while (headerNames.hasMoreElements()) {
-            String headerName = headerNames.nextElement();
-        }
-        return "servlet";
+        return "Purple Cow Server is Operational";
     }
 
-    @Path("/validateCertDate") @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String validateCertificateDate(@Context HttpServletRequest context) {
+    /**
+     * Resource for validating the whether a certificate is expired.
+     * @param context
+     * @return
+     */
+    @GET
+    @Path("/validateCertDate")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response validateCertificateDate(@Context HttpServletRequest context) {
         X509Certificate certs[] =
                 (X509Certificate[])context.getAttribute("javax.servlet.request.X509Certificate");
         X509Certificate certificate = null;
-        if (certs == null)
+        if (certs != null)
              certificate = certs[0];
 
         if (Objects.nonNull(certificate)) {
             ZonedDateTime certificateDate = certificate.getNotAfter().toInstant().atZone(ZoneId.of("UTC"));
-            return certificateDate.isAfter(CURRENT_DATE_TIME) ? INVALID_CERT_MESSAGE : VALID_CERT_MESSAGE;
+            return certificateDate.isAfter(CURRENT_DATE_TIME) ? Response.status(400).entity(INVALID_CERT_MESSAGE).build()
+                    : Response.status(200).entity(VALID_CERT_MESSAGE).build();
         }
         else
-            return CORRUPTED_CERT_MESSAGE;
-
+            return Response.status(400).entity(CORRUPTED_CERT_MESSAGE).build();
     }
 }
